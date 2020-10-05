98684f73783fc5123ff1862bf48478e47e3182f9
Colton Swapp <coltonbswapp@gmail.com>
2020-10-05 16:10:00 -0600

    Sign in, Sign Up, and Log out functionality are all complete. Sorry that it took me a year.

--- ModelControllers/UserController.swift
+++ ModelControllers/UserController.swift
@@ -17,22 +17,28 @@ class UserController {
     // MARK: - Current User
     var currentUser: User?
     
-    func createUser(name: String, email: String, UUID: String) {
+    func createUser(name: String, email: String, UUID: String, completion: @escaping(Bool) -> Void) {
         let newUser = User(name: name, email: email, userId: UUID)
         let db = Firestore.firestore()
         
-        db.collection("users").document(newUser.userId).setData(["name": newUser.name, "email" : newUser.email, "userID": newUser.userId])
-        
-        self.currentUser = newUser
+        db.collection("users").document(newUser.userId).setData(["name": newUser.name, "email" : newUser.email, "userID": newUser.userId]) { (error) in
+            if let error = error {
+                print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+                completion(false)
+            }
+            
+            self.currentUser = newUser
+            completion(true)
+        }
     }
     
-    func retrieveProfile(userId: String, completion: @escaping (User?) -> Void) {
+    func retrieveProfile(userId: String, completion: @escaping (Result<User?, LoginError>) -> Void) {
         
         let db = Firestore.firestore()
         
         db.collection("users").document(userId).getDocument { (snapshot, error) in
             if let error = error {
-                print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+                return completion(.failure(LoginError.thrownError(error)))
             }
             
             if let profile = snapshot?.data() {
@@ -43,11 +49,11 @@ class UserController {
                 
                 let user = User(name: name, email: email, userId: userId)
                 self.currentUser = user
-                completion(user)
+                completion(.success(user))
             }
             
             else {
-                completion(nil)
+                return completion(.failure(LoginError.unableToDecode))
             }
         }
     }
--- ViewControllers/LoginViewController.swift
+++ ViewControllers/LoginViewController.swift
@@ -8,69 +8,113 @@
 import UIKit
 import FirebaseUI
 import Firebase
-import GoogleSignIn
+import FirebaseAuth
+
 
 class LoginViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        if UserController.sharedInstance.currentUser != nil {
+            performSegue(withIdentifier: "toHomeScreenVC", sender: nil)
+        }
+    }
+    
     // MARK: - Actions
-    @IBAction func getStartedButtonTapped(_ sender: Any) {
-        presentEmailGoogleSignIn()
-    }
-    
-    
-    // MARK: - HelperMethods
-    
-    func presentEmailGoogleSignIn() {
-        let authUI = FUIAuth.defaultAuthUI()
+    @IBAction func signInButtonTapped(_ sender: Any) {
+        guard let emailText = emailTextField.text, !emailText.isEmpty,
+              let passwordText = passwordTextField.text, !passwordText.isEmpty else { return }
         
-        
-        if let authUI = authUI {
-            
-//             Set self as the delegate of AuthUI
-            authUI.delegate = self
-            
-            
-            authUI.providers = [FUIEmailAuth(), FUIGoogleAuth()]
-            
-            
-            let authViewController = authUI.authViewController()
-            
-            
-            present(authViewController, animated: true, completion: nil)
-        }
-    }
-}
-
-extension LoginViewController: FUIAuthDelegate {
-    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
-        if let error = error {
-            print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
-        }
-        
-        let user = authDataResult?.user
-        
-        if let user = user {
-            
-            
-            UserController.sharedInstance.retrieveProfile(userId: user.uid) { (foundUser) in
-                if let foundUser = foundUser {
-                    // go home page
-                    self.dismiss(animated: true, completion: nil)
-                }
-                else {
-                    guard let username = user.displayName, let email = user.email else { return }
-                    UserController.sharedInstance.createUser(name: username, email: email, UUID: user.uid)
-                    self.dismiss(animated: true, completion: nil)
-                    
+        Auth.auth().signIn(withEmail: emailText, password: passwordText) { (user, error) in
+            if let error = error {
+                print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+                // Present some sort of could not log in alert
+            }
+            if let user = user?.user {
+                UserController.sharedInstance.retrieveProfile(userId: user.uid) { (result) in
+                    switch result {
+                    case .success(let user):
+                        print("\(user?.name) was successfully signed in.")
+                        self.dismiss(animated: true, completion: nil)
+                    case .failure(let error):
+                        print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+                    }
                 }
             }
-            
         }
+    }
+    
+    @IBAction func signUpButtonTapped(_ sender: Any) {
+        let signUpStoryboard = UIStoryboard(name: "SignUp", bundle: nil)
+        guard let signUpViewController = signUpStoryboard.instantiateInitialViewController() else { return }
+        self.navigationController?.pushViewController(signUpViewController, animated: true)
+        
+    }
+    
+    
+    
+
+    // MARK: - HelperMethods
+    // Keyboard Adjust Frame
+    @objc func adjustForKeyboard(notification: Notification) {
+      guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+      
+      let keyboardScreenEndFrame = keyboardValue.cgRectValue
+      let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+      
+      if notification.name == UIResponder.keyboardWillHideNotification {
+        scrollView.contentInset = .zero
+      } else {
+        scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom + 40, right: 0)
+      }
+      scrollView.scrollIndicatorInsets = scrollView.contentInset
+    }
+    
+    func enableSignInButton() {
+        guard let emailText = emailTextField.text, let passwordText = passwordTextField.text else { return }
+        if !emailText.isEmpty && !passwordText.isEmpty {
+            self.signInButton.setTitleColor(.red, for: .normal)
+            self.signInButton.isEnabled = true
+        } else {
+            self.signInButton.isEnabled = false
+            self.signInButton.setTitleColor(.gray, for: .normal)
+        }
+    }
+    
+    // MARK: - Navigation
+   
+    
+
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if emailTextField.isFirstResponder {
+            return emailTextField.resignFirstResponder()
+        } else {
+            return passwordTextField.resignFirstResponder()
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        enableSignInButton()
     }
 }
